bef06a6b79220d8a227c6f613534b50ee8cde796
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-10-10 16:40:47 +0300

    fix error

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -13,7 +13,7 @@ export const getContactsController = async (req, res) => {
 };
 
 
-export const getContactByIdController = async (req, res) => {
+export const getContactByIdController = async (req, res, next) => {
 
     const { contactId } = req.params;
     const contact = await getContactsById(contactId);
@@ -45,8 +45,7 @@ export const deleteContactController = async (req, res, next) => {
     const contact = await deleteContact(contactId);
 
     if (!contact) {
-        next(createHttpError(404, 'Contact not found'));
-        return;
+        throw createHttpError(404, 'Contact not found');
     };
     res.status(204).send();
 };
@@ -56,8 +55,7 @@ export const patchContactController = async (req, res, next) => {
     const result = await updateContact(contactId, req.body);
 
     if (!result) {
-        next(createHttpError(404, 'Contact not found'));
-        return;
+        throw createHttpError(404, 'Contact not found');
     }
 
     res.json({
@@ -78,8 +76,7 @@ export const patchContactController = async (req, res, next) => {
 //     });
 
 //     if (!result) {
-//         next(createHttpError(404, 'Contact not found'));
-//         return;
+//        throw createHttpError(404, 'Contact not found');
 //     }
 
 //     const status = result.isNew ? 201 : 200;
