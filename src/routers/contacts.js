eab4f28ac87e940ea7cbedb3bfb1da8f623326a8
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-10-22 00:03:28 +0300

    fix patch

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { addNewContactController, deleteContactController, getContactByIdController, getContactsController, patchContactController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactSchema } from '../validation/contacts.js';
+import { createContactSchema, patchContactSchema } from '../validation/contacts.js';
 import { isValidId } from "../middlewares/isValidId.js";
 
 
@@ -12,7 +12,7 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(addNewContactController));
 router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
-router.patch('/contacts/:contactId', validateBody(createContactSchema), isValidId, ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId', validateBody(patchContactSchema), isValidId, ctrlWrapper(patchContactController));
 
 // router.put('/contact/:contactId', ctrlWrapper(upsertContactController));
 
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -22,3 +22,22 @@ export const createContactSchema = Joi.object({
     }),
 });
 
+
+
+export const patchContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20).messages({
+        'string.base': 'Name must be a string',
+        'string.min': 'Name must have at least {#limit} characters',
+        'string.max': 'Name must have at most {#limit} characters',
+    }),
+    phoneNumber: Joi.string().messages({
+        'string.base': 'Phone number must be a string',
+    }),
+    email: Joi.string().email().messages({
+        'string.email': 'Invalid email format',
+    }),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid('work', 'home', 'personal').messages({
+        'any.only': 'Contact type must be one of [work, home, personal]',
+    }),
+}).min(1);
